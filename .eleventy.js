a724a01f7a764a04289ee546fd0d20b7c05e5cca
Joseph McKay <mckay.joey@gmail.com>
2025-04-19 08:17:41 +0100

    Reorganised project structure for Decap CMS and Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("img");
+  // Copy static assets to the output folder
+  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin");
 
   return {
     dir: {
-      input: ".",
-      includes: "_includes",
+      input: "content",       // Source content lives here now
+      includes: "../_includes", // Adjusted to reach _includes at project root
       output: "_site"
     }
   };
